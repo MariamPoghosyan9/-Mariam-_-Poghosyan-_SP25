1a88ebc92470d605c7adfc2398bb96b2f55b4f85
poghosyanmariam44 <poghosyanmariam44@gmail.com>
2025-04-24 21:23:11 +0400

    Add lower case and delete hardcode

--- SQL_DML_Mariam_Poghosyan_HW_Task1.sql
+++ SQL_DML_Mariam_Poghosyan_HW_Task1.sql
@@ -12,7 +12,7 @@ SELECT
     'PG-13'::mpaa_rating,  -- Explicit cast to mpaa_rating
     NOW(), 
     ARRAY['Behind the Scenes', 'Commentary'], 
-    to_tsvector('english', '')  -- Convert empty string to tsvector
+    to_tsvector('english', '') 
 UNION ALL
 SELECT 
     'Fast and Furious', 
@@ -23,10 +23,10 @@ SELECT
     9.99, 
     106, 
     17.99, 
-    'PG-13'::mpaa_rating,  -- Explicit cast to mpaa_rating
+    'PG-13'::mpaa_rating, 
     NOW(), 
     ARRAY['Deleted Scenes', 'Trailer'], 
-    to_tsvector('english', '')  -- Convert empty string to tsvector
+    to_tsvector('english', '')  
 UNION ALL
 SELECT 
     'Good Children Don''t Cry', 
@@ -37,11 +37,10 @@ SELECT
     19.99, 
     96, 
     16.99, 
-    'PG'::mpaa_rating,  -- Explicit cast to mpaa_rating
+    'PG'::mpaa_rating, 
     NOW(), 
     ARRAY['Trailer', 'Behind the Scenes'], 
     to_tsvector('english', 'A touching story of a young girl battling illness and finding strength in friendship.')  -- Example of tsvector conversion with a text
-
 
 
 -- 1.2 Add actors to the 'actor' table
@@ -55,7 +54,6 @@ VALUES
     ('Nils', 'Verkooijen');      -- Good Children Don't Cry
 
 -- 1.3 Link actors to their films in 'film_actor' (dynamic actor_id, film_id)
--- Insert into film_actor linking actors and films dynamically with case-insensitive matching
 INSERT INTO film_actor (actor_id, film_id, last_update)
 SELECT 
     a.actor_id, 
@@ -97,8 +95,8 @@ WHERE customer_id = (
 DELETE FROM payment WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'John' AND last_name = 'Doe');
 DELETE FROM rental WHERE customer_id = (SELECT customer_id FROM customer WHERE first_name = 'John' AND last_name = 'Doe');
 
--- 1.7 Rent the movies and insert payment records (dynamic rental dates and film titles)
 
+-- 1.7 Rent the movies and insert payment records (dynamic rental dates and film titles)
 WITH rentals AS (
     INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id, return_date, last_update)
     SELECT '2017-01-10 10:00:00', i.inventory_id, c.customer_id, 1, '2017-01-17 10:00:00', NOW()
@@ -122,7 +120,6 @@ WITH rentals AS (
 )
 INSERT INTO payment (customer_id, staff_id, rental_id, amount, payment_date)
 SELECT customer_id, 1, rental_id, rental_rate, '2017-01-11' FROM rentals;
-
 
 WITH rentals AS (
     INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id, return_date, last_update)
